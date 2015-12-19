866b27b42612738480f06627eed9728e9e619d28
EikaMikiku <e.i.k.a@outlook.com>
2015-12-19 17:09:31 +0000

    Added help message

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ var Game = (function() {
 	var instance;
 	function createInstance() {
 		function Game(){};
-		var player = new Player(110, 150, 0);
+		var player = new Player(15, 15, Math.PI/2);
 		var mapManager = MapManager(player);
 		Game.frameCount = 0;
 		Game.npcs = generateNpcsArray(mapManager.mapLevel);
